b050243065f46c577549a12b2fa2d8aa87f7a518
Austin West <korruptedanomaly@gmail.com>
2019-05-22 10:33:28 -0600

    File Hierarchy organized into folders

---
+++ RedditOBJC/RedditOBJC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditOBJC
+//
+//  Created by Austin West on 5/22/19.
+//  Copyright © 2019 Austin West. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
